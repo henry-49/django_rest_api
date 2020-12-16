4d787162b93214905ae44ec396f5a518616bd052
henry-49 <henryswebdevelopment@gmail.com>
2020-12-16 19:38:24 +0000

    Modified task urls.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'created_at', 'updated_at')
+
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
+
